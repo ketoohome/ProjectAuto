525be2df372c50f3b50b7f369a4789dfe1ca7b20
ketoohome <35617875+ketoohome@users.noreply.github.com>
2018-02-03 13:07:28 +0800

    材质和场景测试
    增加了草地场景

---
+++ Project_Auto/Assets/Test/control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class control : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        NavMeshAgent agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
